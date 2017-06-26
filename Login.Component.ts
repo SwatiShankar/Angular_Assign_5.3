cc4039ff12f262a5ed0ddc0a2e658564afe6784d
SwatiShankar <swati.shankar22@gmail.com>
2017-06-26 21:21:32 +0530

    Add files via upload
    3rd assignment of 5th session

---
+++ Login.Component.ts
@@ -0,0 +1,12 @@
+import {LoginComponent} from '@angular/core';
+
+@LoginComponent({
+  selector: '',
+  templateUrl: './header.log.html',
+  styleUrls: ['./header.login.css]
+})
+export class RootComponent{
+  
+  constructor(){}
+
+}
